b96c873d021f52cd4680c1ad8835e4df2eaef5e5
Paul Henke <laundrysock@gmail.com>
2017-11-06 12:21:21 -0800

    Add --race cmd line option
    Note this slows things down surprisingly

--- ingenium/name_maker.py
+++ ingenium/name_maker.py
@@ -24,18 +24,39 @@ FirstNameStats = collections.namedtuple('FirstNameStats', ['name', 'gender', 'co
 SurnameStats = collections.namedtuple('SurnameStats', ['name', 'rank', 'count', 'prop100k', 'cum_prop100k', 'pctwhite',
                                                        'pctblack', 'pctapi', 'pctaian', 'pct2prace', 'pcthispanic'])
 
+race_arg_map = {
+    'white': 'pctwhite',
+    'black': 'pctblack',
+    'api': 'pctapi',
+    'indian': 'pctaian',
+    'hispanic': 'pcthispanic',
+    'multi': 'pct2prace',
+}
+
 
 def build_parser():
     argparser = argparse.ArgumentParser(description='Generate people names')
     argparser.add_argument('gender', type=str, help='gender')
     argparser.add_argument('year', type=int, help='year of birth')
-    argparser.add_argument('count', type=int, help='number of names to generate (required)')
+    argparser.add_argument('count', type=int, help='number of names to generate')
 
+    argparser.add_argument('--weighted', '-w', dest='weighted', action='store_true')
     argparser.add_argument('--first-initial', '-f', dest='first_initial', type=str, help='first initial')
     argparser.add_argument('--surname-initial', '-s', dest='surname_initial', type=str, help='surname initial')
-    argparser.add_argument('--weighted', '-w', dest='weighted', action='store_true')
+    argparser.add_argument('--race', '-r', dest='race', type=str, help='race')
 
     return argparser
+
+
+def race_percent(surname_stats, race):
+    if race in race_arg_map:
+        try:
+            return float(surname_stats.__getattribute__(race_arg_map[race]))
+        except ValueError:
+            return 0.0
+    else:
+        print("No race found for {race}".format(race=race))
+        return 1.0
 
 
 def load_first_names_by_year(year, weighted=False, initial=None):
@@ -56,7 +77,7 @@ def load_first_names_by_year(year, weighted=False, initial=None):
     return names
 
 
-def load_surnames(weighted=False, initial=None):
+def load_surnames(weighted=False, initial=None, race=None):
     surname_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/surnames_by_race.zip")
     surnames_zip = zipfile.ZipFile(surname_file)
 
@@ -65,15 +86,16 @@ def load_surnames(weighted=False, initial=None):
     for line in surnames_zip.read("app_c.csv").splitlines()[1:]:
         surname_stats = SurnameStats(*line.decode().split(','))
         relative_name_count = int(int(surname_stats.count) / MIN_SURNAME_COUNT) if weighted else 1
+        relative_name_count *= race_percent(surname_stats, race) if race else 1
 
         if not initial or (initial and surname_stats.name.lower().startswith(initial.lower())):
-            surnames += [surname_stats.name for _ in range(relative_name_count)]
+            surnames += [surname_stats.name for _ in range(int(relative_name_count))]
 
     return surnames
 
 
 def generate_names(args):
-    surnames = load_surnames(weighted=args.weighted, initial=args.surname_initial)
+    surnames = load_surnames(weighted=args.weighted, initial=args.surname_initial, race=args.race)
     first_names = load_first_names_by_year(args.year, weighted=args.weighted, initial=args.first_initial)
 
     for _ in range(args.count):
